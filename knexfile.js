48d830126aa2cd073677be5f8ce7c6dd8872ab72
Gaurav <program.drgk@gmail.com>
2024-04-11 16:31:50 -0700

    knexfile db title reverted to original

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    db: process.env.DB_NAME,
+    database: process.env.DB_NAME,
     // charset: "utf8",
   },
 };
