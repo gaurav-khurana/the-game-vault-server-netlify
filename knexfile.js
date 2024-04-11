a025ea99e169246fa1f3ebd9ad79c5766c6a55be
Gaurav <program.drgk@gmail.com>
2024-04-11 16:28:02 -0700

    knexfile db order changed

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,9 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     // charset: "utf8",
   },
 };
