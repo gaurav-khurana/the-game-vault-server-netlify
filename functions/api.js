b20a33bb0ae4b1ab8c621e343c0e22aedfb9f854
Gaurav <program.drgk@gmail.com>
2024-04-15 22:17:10 -0700

    code modified to deploy on Netlify

--- functions/api.js
+++ functions/api.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
+const serverless = require("serverless-http");
+const router = express.Router();
+
 const { PORT } = process.env;
 
 // middleware for cors
@@ -17,10 +20,16 @@ app.use(express.json());
 app.use("/static-files", express.static("public"));
 
 // routes - xbox & PS games
-const xboxPsRoutes = require("./routes/routes-xbox-ps");
+const xboxPsRoutes = require("../routes/routes-xbox-ps");
 app.use("/games", xboxPsRoutes);
+app.use("/", (req, res) => {
+  res.send("Server Deployed successfully");
+});
 
 // listen to Port
 app.listen(PORT, () => {
   console.log("Listening to server on ", PORT);
 });
+
+app.use("/.netlify/functions/api", xboxPsRoutes);
+module.exports.handler = serverless(app);
